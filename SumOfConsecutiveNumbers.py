c79b2eb01d1f0bb306169cddab94ed496e29d215
Daniel <daniel-rupp@outlook.de>
2021-08-30 12:02:23 +0200

    changed

--- SumOfConsecutiveNumbers.py
+++ SumOfConsecutiveNumbers.py
@@ -2,7 +2,7 @@
 #? Sum of Consecutive Numbers
 #* No one likes homework, but your math teacher has given you an assignment to find the sum of the first N numbers.
 
-#* Let’s save some time by creating a program to do the calculation for you!
+#* Lets save some time by creating a program to do the calculation for you!
 
 #* Take a number N as input and output the sum of all numbers from 1 to N(including N).
 
--- TestFileForGit.py
+++
@@ -1,5 +0,0 @@
-import random
-
-hi = 23.05
-
-print(random(1,2,3,4,5,6,7,8,9, "\n"))
