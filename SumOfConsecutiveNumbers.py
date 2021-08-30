8f53a2ab1521173ddd1169b4c42cf4bb504bfe76
Daniel <daniel-rupp@outlook.de>
2021-08-30 18:05:55 +0200

    a java project

--- SumOfConsecutiveNumbers.py
+++ SumOfConsecutiveNumbers.py
@@ -1,28 +1,31 @@
 
-#? Sum of Consecutive Numbers
-#* No one likes homework, but your math teacher has given you an assignment to find the sum of the first N numbers.
 
-#* Lets save some time by creating a program to do the calculation for you!
+N = 100 
+print (sum(range(1, N+1)), "\n") #? the string caused error
 
-#* Take a number N as input and output the sum of all numbers from 1 to N(including N).
+g = range(2,10)
+for i in g:
+    print(i)
 
-#* Sample Input
-#* 100 
+h = range(3,7)
+for i in h:
+    print(i)
 
-#* Sample Output
-#* 5050
+#NOTE: range(2,10); start from 2 end before 10. same as range(3,7)
+#?okay what next?
+#* hmmmmm
+#? i think it needs more 
 
-#* Explanation: The sum of all numbers from 1 to 100 is equal to 5050.
 
-#NOTE:
-#/ You can iterate over a range and calculate the sum of all numbers in the range.
-#/ Remember, range(a, b) does not include b, thus you need to use b+1 to include b in the range.
 
-#
-N = int(input())
-#how about now?
 
-#!your code goes here
+
+
+
+
+
+
+
 
 
 
