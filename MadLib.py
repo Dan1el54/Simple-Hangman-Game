781497e30f0f21f86b1430504a3e135192fba828
Daniel <daniel-rupp@outlook.de>
2021-09-02 14:40:10 +0200

    Co-authored-by: tere1120

--- MadLib.py
+++ MadLib.py
@@ -1,12 +1,5 @@
 #? it worksss
-#? for new projectt
-#* you know that its just a file?
-#? wdym? oh okay i understand
-#* what we are using right now is a workspace where all the files in
-#* you cant do a workspace in a workspace
-#? huh?? but the workspace is in there. It's on your file
-#* no what file you mean
-#? myroadto
-#* yeah thats kinda like the workspace
-#? -__-
-#* now its a local workspace
+
+#? yes. you have more interesting project?
+#? this one pretty easy. ive see it on google
+#* idk which one you wanna do now
