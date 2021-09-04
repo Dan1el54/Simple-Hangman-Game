6ed5b8040595c0b2da2ff6a35d5f67f68d448ce3
Daniel <daniel-rupp@outlook.de>
2021-09-04 20:36:43 +0200

    CS

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyRoadToWebDevAndStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+        }
+    }
+}
