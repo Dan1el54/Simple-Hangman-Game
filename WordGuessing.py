3ba4fc8ecea8541981d52248de9969c79a26be50
theresia <theresia.tobing20@outlook.com>
2021-09-12 14:55:22 +0700

    new def

--- WordGuessing.py
+++ WordGuessing.py
@@ -4,33 +4,56 @@ import random
 import time
 import Main
 
-
-
 word = random.choice(word)
 length = len(word)
-check = list(length * "_")
+check = length * ['_']
+wrong_guess = 0
+
 print(check)
 lives = 6
 gameWon = False
-
 
 def Won():
   if gameWon == True:
     print("Congratulation! You're a champ")
     time.sleep(0.1)
 
+def gameOver():
+  gameWon = False
+  print("Game Over")
+  time.sleep(1)
+  print ("The word is ", word)
+  time.sleep(0.1)
+  Main.response()
+
+def right():
+  for i in check:
+    print(i, end = " ")
+   
 while gameWon is False or lives > 0:
   guess = input ("guess a letter or word: ")
   time.sleep(0.1)
+  if guess in word:
+    Won
+  if len(guess) == 1 and guess.isalpha:
+    if guess not in word:
+      print ("you guessed wrong")
+      lives -= 1
+      print ("you have remaining lives: ", lives)
+    if lives == 0:
+      gameOver()
+  
+  for i in word:
+    if guess in word:
+      index=0
+      if i == guess:
+        check[index]=guess
+        index += 1
+    right()
+      
+  
+    
 
-  if len(guess) == 1 and guess in word:
-    for i in range (0, length):
-      check = word[i]
-      check[i] = guess
-  elif guess not in word:
-        lives -= 1
-  elif lives == 0:
-      print("Game Over")
-      time.sleep(0.1)
-      Main.response()
-
+      
+  
+      
