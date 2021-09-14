13be5f8848314eae5516e0ef92be80eef1cf0c60
theresia <theresia.tobing20@outlook.com>
2021-09-14 18:06:34 +0700

    Code still not reveal letter

--- WordGuessing.py
+++ WordGuessing.py
@@ -3,34 +3,49 @@ from wordlist import word
 import random
 import time
 import Main
+from image import hangman
 
 
-
+print (hangman[0])
 word = random.choice(word)
 length = len(word)
-check = list(length * "_")
+check = length * " _ "
 print(check)
+guessed_letter = []
 lives = 6
+wrong = 0
 gameWon = False
 
 
 def Won():
-  if gameWon == True:
-    print("Congratulation! You're a champ")
-    time.sleep(0.1)
+  if gameWon == False:
+    print ("Game Over")
+    time.sleep(0.5)
+    print ("the word is:", word)
+  else:
+    print ("Congratulation! You are a Champ")
 
-while gameWon is False or lives > 0:
+
+
+while lives > 0:
   guess = input ("guess a letter or word: ")
   time.sleep(0.1)
-
-  if len(guess) == 1 and guess in word:
-    for i in range (0, length):
-      check = word[i]
-      check[i] = guess
-  elif guess not in word:
-        lives -= 1
-  elif lives == 0:
-      print("Game Over")
-      time.sleep(0.1)
-      Main.response()
-
+  if guess == word:
+    gameWon = True
+    Won()
+    
+  for i in range(length):
+    right_letter = word[i] #! New variable. contains only guessed letter that store in word
+    if guess == word [i]:
+      guessed_letter == guess #! still not working
+    elif guess not in word:
+      wrong += 1
+      print (hangman [wrong])
+      print ("you guess wrong")
+      lives -= 1
+      print ("you have remaining lives: ",lives)
+      if lives == 0:
+        gameWon = False
+        Won()
+      break
+  
