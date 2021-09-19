e3c7f45c32861f9d03bb21a74ef02968170eb171
Daniel <daniel-rupp@outlook.de>
2021-09-19 15:16:59 +0200

    tere

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,72 @@
+#? this is the hangman game
 from tkinter import *
+from wordlist import word
 import random
+import time
+from image import hangman
+import Main
+
+#! can you run this?
+
+print (hangman[0])
+word = random.choice(word)
+length = len(word)
+check = length * " _ "
+print(check)
+print ("length of the word is", length)
+list_of_guess = []
+lives = 6
+wrong = 0
+gameEnd = False #we need to define this. when the gameEnd is true or false
+
+#! which one is the hangman file? this one
 
 
-def firstGame():
-    return print("Hangman " + random(1,50))
-
-
-
-
-
+def gameWon(gameEnd):
+    if guess == word:
+        gameEnd = True
+        print ("Congratulation! You won!")
+    if lives == 0:
+        gameEnd = True
+        print ("Game Over. The word is:", word)
+    else:
+        gameEnd = False
+  
+while gameEnd == False and lives > 0:
+    guess = input("guess a letter or a word: ")
+    time.sleep (0.1)
+    
+    if guess in list_of_guess:
+        print ("You already guess that letter/word. Enter a new letter or word")
+        list_of_guess.append(guess)
+        break
+  
+    if len(guess) == 1:
+        if guess.isnumeric():
+            print("You enter a numeric! Enter only a letter")
+        elif guess == ["?", ">", "!", "#", "=", "%", "*", "$", ".", ","]:
+            print("Input invalid. Enter a letter or a word") 
+        else:
+            for i in range(length):
+                right_letter = i
+                if word[i] == guess: #? this is not working at all
+                    print(i, end = "")  
+                if guess not in word:
+                    lives -= 1
+                    print ("Wrong guess. Remaining lives:", lives)
+                    wrong += 1
+                    print (hangman[wrong])
+        
+    else:
+        while len(guess) == 0 or len(guess) > 1:
+            if guess not word:
+                print ("enter only 1 letter or a whole word") 
+            if len(guess) == len(word):
+                if guess == word:
+                    gameWon()
+                else:
+                    wrong += 1
+                    print (hangman[wrong])
+                    lives -= 1
+                    print("Wrong guess. Remaining lives:", lives)
+    gameWon()
--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
+#? just keep this as introoooo
+#? why yoou change the main fileeee
 
 from ast import Break
 from tkinter import *
-import Hangman
-import NumberGuessing
-import WordGuessing
+import HangMan # let's just change the hangman with other game and change this one with hangman
 import time
-import WordGuessing
+import NumberGuessing
 
 print(" Welcome to our GameHub! \n")
 time.sleep(0.1)
@@ -20,19 +20,19 @@ gameNR = 0
 ans=True
 ans = print("""      
       1.Hangman
-      2.Guess the Number
-      3.Guess the Word
+      2.NumberGuessing
+      3.WordGuessing
       4.Exit/Quit\n
       """)
 gameNR = input("What would you like to do? ", )
 
 if gameNR == "1":
-      Hangman()
+      HangMan()
       time.sleep(0.1)
 elif gameNR == "2":
       NumberGuessing()
 elif gameNR == "3":
-      WordGuessing
+      print ("0")
 elif gameNR == "4":
       print("\n Goodbye \n")
       ans = False
