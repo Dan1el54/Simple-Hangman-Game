cc4ebf7ccb83540ffaba309adc2dbfa9852c830c
Daniel <daniel-rupp@outlook.de>
2021-09-19 15:23:18 +0200

    tere

--- Hangman.py
+++ Hangman.py
@@ -1,10 +1,11 @@
 #? this is the hangman game
+from multiprocessing.connection import wait
 from tkinter import *
 from wordlist import word
 import random
 import time
 from image import hangman
-import Main
+
 
 #! can you run this?
 
@@ -20,8 +21,11 @@ wrong = 0
 gameEnd = False #we need to define this. when the gameEnd is true or false
 
 #! which one is the hangman file? this one
-
-
+#? and how about that HangMan file?
+#* this is the HangMan file. oh youre r runnnnnight XD
+#? can you run this? can you run again?
+#* i did
+#* you should close all open files and open them again from the workspace
 def gameWon(gameEnd):
     if guess == word:
         gameEnd = True
@@ -55,11 +59,11 @@ while gameEnd == False and lives > 0:
                     lives -= 1
                     print ("Wrong guess. Remaining lives:", lives)
                     wrong += 1
-                    print (hangman[wrong])
+                    break            #print (hangman[wrong])
         
     else:
         while len(guess) == 0 or len(guess) > 1:
-            if guess not word:
+            if guess not in word:
                 print ("enter only 1 letter or a whole word") 
             if len(guess) == len(word):
                 if guess == word:
@@ -69,4 +73,8 @@ while gameEnd == False and lives > 0:
                     print (hangman[wrong])
                     lives -= 1
                     print("Wrong guess. Remaining lives:", lives)
-    gameWon()
+                    break                    
+                    
+gameWon()
+
+#* wait i need to eat now and i will make a new live share to make all changes clear.
