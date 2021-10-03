9caab1d004f3a846fb28cd3e067c3ee1a9095424
Daniel <daniel-rupp@outlook.de>
2021-10-03 16:36:07 +0200

    daniel

--- testing/Test_Hangman.py
+++ testing/Test_Hangman.py
@@ -1,6 +1,5 @@
 
 #note: HANGMAN TESTFILE
-from tkinter import *
 import wordlist
 import random
 import time
@@ -16,7 +15,7 @@ print("length of the word is", length)
 list_of_guess = []
 lives = 6
 wrong = 0
-#gameEnd = False  #note: we need to define this. When the gameEnd is True or False
+gameEnd = False  #note: we need to define this. When the gameEnd is True or False
 
 
 def gameEnd(gameEnd):
@@ -27,11 +26,13 @@ def gameEnd(gameEnd):
 
 def gameWon():  # note: name of define is gameWon. gameEnd is true if:
     if guess == word:  # note: True if guess is word: Win. Code finish running
-        gameEnd = True
         print("Congratulation! You won!")
-    elif lives == 0:  # note: True if lives = 0: lose. Code finish running
         gameEnd = True
+        
+    elif lives == 0:  # note: True if lives = 0: lose. Code finish running
         print("Game Over. The word is:", word)
+        gameEnd = True
+        
     else:  # note: False if anything else other than those 2 True condition. Then code while loop will run.
         gameEnd = False
 
@@ -47,12 +48,12 @@ while gameEnd == False and lives > 0:
     elif len(guess) == 0:  # note: condition 2: if guess is "(no letter)", lives will not reduce
         print("Enter a letter or a word")
 
-    elif len(guess) == 1:  # note: condition 3: length of guess is exactly 1 letter
+    elif len(guess) == 1 and guess.isnumeric():  # note: condition 3: length of guess is exactly 1 letter
         if guess.isnumeric():  # note: UNDER CONDITION 3: BUT in numeric. Will not reduce lives
             print("You enter a numeric! Enter only a letter")
-        elif guess == ["?", ">", "!", "#", "=", "%", "*", "$", ".", ","]:  # warning: not working
+    elif guess == ["?", ">", "!", "#", "=", "%", "*", "$", ".", ","]:  # warning: not working
             print("Input invalid. Enter a letter or a word")
-        else:  # note: UNDER CONDITION 3: letter is alphabet
+    elif i in range(length):  # note: UNDER CONDITION 3: letter is alphabet
             for i in range(length):
                 right_letter = i
                 if word[i] == guess:  # warning: not working
@@ -64,21 +65,23 @@ while gameEnd == False and lives > 0:
                     print(image.hangman[wrong])
                     break
 
-    else:  # note: condition 4, if guess length is as much as word length
-        while len(guess) > 1:
+    elif len(guess) > 1: # note: condition 4, if guess length is as much as word length
+            print("ffff")
             # note: UNDER CONDITION 4: ONLY works if length guess is exactly as much as length word
-            if len(guess) == len(word):
-                if guess == word:  # note: > guess is word: win
-                    gameWon()
-                else:  # note: UNDER CONDITION 4: guess is not word: lose
-                    wrong += 1
-                    print(image.hangman[wrong])
-                    lives -= 1
-                    print("Wrong guess. Remaining lives:", lives)
-                    gameWon()
+    elif len(guess) == len(word):
+        print("kkkk")
+    elif guess == word:  # note: > guess is word: win
+        gameWon()
+    elif guess != word:  # note: UNDER CONDITION 4: guess is not word: lose
+        wrong += 1
+        print(image.hangman[wrong])
+        lives -= 1
+        print("Wrong guess. Remaining lives:", lives)
+        gameWon()
             # note: UNDER CONDITION 4: length guess is more than 1 (but not exactly as much as word length)
-            if guess not in word:
-                print("enter only 1 letter or a whole word")
-                break
+    elif guess not in word:
+        print("enter only 1 letter or a whole word")
+        break
     # note: append function will add variable (guess) to the list_of_guess. have to be added at the last code so that the first guess will not be filtered
-    list_of_guess.append(guess)
+list_of_guess.append(guess)
+
