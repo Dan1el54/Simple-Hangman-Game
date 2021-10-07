ae937f37391037a53f0672ebe2f81660ed0e2567
Daniel <daniel-rupp@outlook.de>
2021-10-07 19:38:40 +0200

    tere1120

--- testing/Test_Hangman.py
+++ testing/Test_Hangman.py
@@ -1,88 +1,79 @@
 
-#note: HANGMAN TESTFILE
+# note: HANGMAN TESTFILE
+
 import wordlist
 import random
 import time
 import image
+import Main
 
-#+ you there?
-
-print(image.hangman[0])
-word = random.choice(wordlist.word)
-length = len(word)
-check = length * " _ "
-print(check)
-print("length of the word is", length)
-list_of_guess = []
-lives = 6
-wrong = 0
-gameEnd = False  #note: we need to define this. When the gameEnd is True or False
-
-
-def gameEnd(gameEnd):
-    if gameEnd == True:
-        print(" You Won! ")
+#todo: we need to figure out how to import stuff from a other folder.
 
 
 
-def gameWon():  # note: name of define is gameWon. gameEnd is true if:
-    if guess == word:  # note: True if guess is word: Win. Code finish running
+def attr():
+    print(image.hangman[0])
+    word = random.choice(wordlist.list_of_word)
+    length = len(word)
+    check = length * " _ "
+    list_of_guess = []
+    lives = 6
+    wrong = 0
+    return (length, check, list_of_guess, lives, wrong, word)
+
+def gameStart():
+    print(attr.check)
+    print("length of the word is", attr.length)
+    return attr.length
+
+def gameWon():  
+    if attr.guess == attr.word: 
         print("Congratulation! You won!")
-        gameEnd = True
-        
-    elif lives == 0:  # note: True if lives = 0: lose. Code finish running
-        print("Game Over. The word is:", word)
-        gameEnd = True
-        
-    else:  # note: False if anything else other than those 2 True condition. Then code while loop will run.
-        gameEnd = False
+    elif attr.lives == 0: 
+        print("Game Over. The word is:", attr.word)
+    end()
 
+def end():
+    yes = 1
+    no = 0
+    inp = input("You want to go back to the Main Menu?")
+    if inp == yes:
+        Main()
+    else:
+        exit()
 
-while gameEnd == False and lives > 0:
-    guess = input("guess a letter or a word: ")
-    time.sleep(0.1)
-
-    # note: condition 1: guess word that already in the list will not reduce lives and return (print)
-    if guess in list_of_guess:
-        print("You already guess that letter/word. Enter a new letter or word")
-
-    elif len(guess) == 0:  # note: condition 2: if guess is "(no letter)", lives will not reduce
+def invalid():
+    if attr.guess.isnumeric():
+        print("You enter numeric! Only letters allowed")
+    elif len(attr.guess) == 0:  
         print("Enter a letter or a word")
+    else:
+        print("Input invalid. Enter a letter or a word")
 
-    elif len(guess) == 1 and guess.isnumeric():  # note: condition 3: length of guess is exactly 1 letter
-        if guess.isnumeric():  # note: UNDER CONDITION 3: BUT in numeric. Will not reduce lives
-            print("You enter a numeric! Enter only a letter")
-    elif guess == ["?", ">", "!", "#", "=", "%", "*", "$", ".", ","]:  # warning: not working
-            print("Input invalid. Enter a letter or a word")
-    elif i in range(length):  # note: UNDER CONDITION 3: letter is alphabet
-            for i in range(length):
-                right_letter = i
-                if word[i] == guess:  # warning: not working
+def gamePlay():
+    while attr.lives > 0: 
+        guess = input("guess a letter or a word: ") 
+        time.sleep(0.1)                             
+        if attr.guess in attr.list_of_guess:
+            print("You already guess that letter/word. Enter a new letter or word")
+        elif len(attr.guess) >= 1: #warning: this will not works
+            for i in range(attr.length):
+                right_letter = ""
+                if attr.word[i] == guess:  
+                    guess = right_letter
                     print(i, end="")
-                if guess not in word:
-                    lives -= 1
-                    print("Wrong guess. Remaining lives:", lives)
-                    wrong += 1
-                    print(image.hangman[wrong])
-                    break
-
-    elif len(guess) > 1: # note: condition 4, if guess length is as much as word length
-            print("ffff")
-            # note: UNDER CONDITION 4: ONLY works if length guess is exactly as much as length word
-    elif len(guess) == len(word):
-        print("kkkk")
-    elif guess == word:  # note: > guess is word: win
-        gameWon()
-    elif guess != word:  # note: UNDER CONDITION 4: guess is not word: lose
-        wrong += 1
-        print(image.hangman[wrong])
-        lives -= 1
-        print("Wrong guess. Remaining lives:", lives)
-        gameWon()
-            # note: UNDER CONDITION 4: length guess is more than 1 (but not exactly as much as word length)
-    elif guess not in word:
-        print("enter only 1 letter or a whole word")
-        break
-    # note: append function will add variable (guess) to the list_of_guess. have to be added at the last code so that the first guess will not be filtered
-list_of_guess.append(guess)
+                    print()
+                else:
+                    attr.lives -= 1
+                    print("Wrong guess. Remaining lives:", attr.lives)
+                    attr.wrong += 1
+                    print(image.hangman[attr.wrong])
+                    attr()
+                    
+    attr.list_of_guess.append(attr.guess)
+    gameWon()         
+    invalid()
+gameStart()
+gamePlay()
+# note: append function will add variable (guess) to the list_of_guess. have to be added at the last code so that the first guess will not be filtered
 
--- wordlist.py
+++ wordlist.py
@@ -1,2 +1,3 @@
-word = ["january", "february", "flower", "tree", "cloud", "snow", "book", "picture", "laptop", "sheep", 
+
+list_of_word = ["january", "february", "flower", "tree", "cloud", "snow", "book", "picture", "laptop", "sheep", 
 "printer", "phone", "pencil", "mouth", "camera", "glasses", "telenovela", "television", "rain"]
