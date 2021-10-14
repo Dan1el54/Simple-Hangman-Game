0a9e982f27164e0663a703739f9965015dc47b42
Daniel <daniel-rupp@outlook.de>
2021-10-14 17:55:37 +0200

    Update Test_Hangman.py

--- testing/Test_Hangman.py
+++ testing/Test_Hangman.py
@@ -26,10 +26,10 @@ def gameStart():
     print("length of the word is", attr.length)
     return attr.length
 
-def gameWon():  
-    if attr.guess == attr.word: 
+def gameWon():
+    if attr.guess == attr.word:
         print("Congratulation! You won!")
-    elif attr.lives == 0: 
+    elif attr.lives == 0:
         print("Game Over. The word is:", attr.word)
     end()
 
@@ -45,21 +45,21 @@ def end():
 def invalid():
     if attr.guess.isnumeric():
         print("You enter numeric! Only letters allowed")
-    elif len(attr.guess) == 0:  
+    elif len(attr.guess) == 0:
         print("Enter a letter or a word")
     else:
         print("Input invalid. Enter a letter or a word")
 
 def gamePlay():
-    while attr.lives > 0: 
-        guess = input("guess a letter or a word: ") 
-        time.sleep(0.1)                             
+    while attr.lives > 0:
+        guess = input("guess a letter or a word: ")
+        time.sleep(0.1)
         if attr.guess in attr.list_of_guess:
             print("You already guess that letter/word. Enter a new letter or word")
         elif len(attr.guess) >= 1: #warning: this will not works
             for i in range(attr.length):
                 right_letter = ""
-                if attr.word[i] == guess:  
+                if attr.word[i] == guess:
                     guess = right_letter
                     print(i, end="")
                     print()
@@ -69,11 +69,10 @@ def gamePlay():
                     attr.wrong += 1
                     print(image.hangman[attr.wrong])
                     attr()
-                    
+
     attr.list_of_guess.append(attr.guess)
-    gameWon()         
+    gameWon()
     invalid()
 gameStart()
 gamePlay()
 # note: append function will add variable (guess) to the list_of_guess. have to be added at the last code so that the first guess will not be filtered
-
